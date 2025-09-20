b25a1b0a3c5a9c21608523346af3caefd2efa725
akashpk41 <akashpk741@gmail.com>
2025-09-20 16:58:07 +0600

    images and router page setup done

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -1,8 +1,19 @@
 import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
+import AuthLayout from "../Layouts/AuthLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <HomeLayout />,
+  },
+
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+  },
+  {
+    path: "/news",
+    element: <h1>News</h1>,
   },
 ]);
